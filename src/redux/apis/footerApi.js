346fa557a74a35fd37a307592a52238731d6a86e
komal8683 <komal.j8683@gmail.com>
2026-01-27 14:57:16 +0530

    fix api baseUrl

--- src/redux/apis/footerApi.js
+++ src/redux/apis/footerApi.js
@@ -1,3 +1,47 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// export const footerApi = createApi({
+//   reducerPath: "footerApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: import.meta.env.VITE_BACKEND_URL
+//       ? import.meta.env.VITE_BACKEND_URL + "/api/footer"
+//       : "http://localhost:5000/api/footer",
+//     credentials: "include",
+//   }),
+//   tagTypes: ["Footer"],
+
+//   endpoints: (builder) => ({
+//     /* 🌍 Public */
+//     getPublicFooter: builder.query({
+//       query: () => "/public",
+//       providesTags: ["Footer"],
+//     }),
+
+//     /* 🔐 Admin */
+//     getAdminFooter: builder.query({
+//       query: () => "/",
+//       providesTags: ["Footer"],
+//     }),
+
+//     saveFooter: builder.mutation({
+//       query: (data) => ({
+//         url: "/",
+//         method: "PUT",
+//         body: data,
+//       }),
+//       invalidatesTags: ["Footer"],
+//     }),
+//   }),
+// });
+
+// export const {
+//   useGetPublicFooterQuery,
+//   useGetAdminFooterQuery,
+//   useSaveFooterMutation,
+// } = footerApi;
+
+
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const footerApi = createApi({
